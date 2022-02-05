fcdc53e379b96888f7e18cbcedbd994131bade63
Jere Ritchie <jereritchie@gmail.com>
2022-02-04 22:56:04 -0500

    cli boilerplate

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # Features to build!
 - define and run sql script packages
 - conditionally run packages based on environment variables
+
+
+## To use:
+Access help
+```shell
+python3 dbmasta -h
+```
--- dbmasta/__main__.py
+++ dbmasta/__main__.py
@@ -1,1 +1,12 @@
-# TODO code you fool!
+# standard imports
+
+# external imports
+
+# project imports
+import commands
+
+
+if commands.args.subcommand is None:
+    commands.cli.print_help()
+else:
+    commands.args.func(commands.args)
