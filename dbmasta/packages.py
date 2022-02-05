815993df97ac4ba032d6123ed3c22e84193a1a23
Jere Ritchie <jereritchie@gmail.com>
2022-02-05 13:58:46 -0500

    stub out pack and mani

---
+++ dbmasta/packages.py
@@ -0,0 +1,16 @@
+# standard imports
+
+# external imports
+
+# project imports
+
+
+class package(object):
+
+    def __init__(self):
+        pass
+
+class manifest(object):
+
+    def __init__(self):
+        pass
