0ca5ac53650809f21fe414391d7500c0efe504fe
Jere Ritchie <jereritchie@gmail.com>
2022-02-05 13:58:12 -0500

    init manifest

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Features to build!
 - define and run sql script packages
-- manifest managed via environment variables
+- json manifests managed via environment variables
 - auto generate guid for modules
 - migration changelog written to target database
 - connection manager
@@ -11,4 +11,14 @@
 Access help
 ```shell
 python3 dbmasta -help
+```
+
+Create package
+```shell
+python3 dbmasta createpackage devdb sqlite --environments=dev,qa,uat,prod
+```
+
+Add module to package
+```shell
+python3 dbmasta createpackage devdb <module_name>
 ```
--- dbmasta/commands.py
+++ dbmasta/commands.py
@@ -2,43 +2,69 @@
 from argparse import ArgumentParser
 import pathlib as pl
 from typing import List
+import json
 
 # external imports
-import typer
+import typer # using for quick build of cli prototype
 
 # project imports
 
 
-
+#################################
+## Module level variables
+#################################
 app = typer.Typer()
+pack_folder = pl.Path().cwd().joinpath("packages")
 
-pack_path = pl.Path().cwd().joinpath("packages")
+manifest_template = {}
 
+#################################
+## Module level functions
+#################################
+
+#################################
+## CLI Commands
+#################################
 @app.command()
 def createpackage(
     packagename: str,
-    dbtype: str="sqlite"
+    dbtype: str,
+    environments: str="dev,qa,uat,prod"
 ):
     typer.echo(f"Creating {packagename} package")
     typer.echo(f"{packagename}'s Database type is {dbtype}")
+    typer.echo(f"{packagename}'s environments include {environments.split(',')}")
      # check if package exists
-    if pack_path.joinpath(packagename).exists:
-        raise FileExistsError(f"A package with the name {packagename} already exists at {pack_path.joinpath(packagename)}.")
+    if pack_folder.joinpath(packagename).exists():
+        raise FileExistsError(f"A package with the name {packagename} already exists at {pack_folder.joinpath(packagename)}.")
 
     # initialize package folder
-    with open(pack_path.joinpath(packagename, "__init__.py"), "w") as f:
+    pack_folder.joinpath(packagename).mkdir()
+    pack_path = pack_folder.joinpath(packagename)
+    with open(pack_path.joinpath("__init__.py"), "w") as f:
         f.write("# auto generated package initializer")
+    
+    # initialize package manifest
+    env_list = environments.split(",")
+    manifest_template["environments"] = env_list
+    for e in env_list:
+        manifest_template[e] = {
+            "connection": {},
+            "migrations": []
+        }
+    with open(pack_path.joinpath("manifest.json"), "w") as f:
+        json.dump(manifest_template, f, indent=4)
 
 
 
 @app.command()
-def createmodule(
-    packagename: str, 
-    modulename: str,
-    environments: List[str]
+def createmigration(
+    migrationname: str,
+    packagename: str
 ):
-    typer.echo(f"Creating {modulename} in {packagename} package...")
-    typer.echo(f"{modulename} is to run in the following environments {environments}")
+    typer.echo(f"Creating {migrationname} in {packagename} package...")
+    # TODO read package numbers and increment +1 to incoming migration
+    # TODO initialize SQL file
 
 
 if __name__ == "__main__":
